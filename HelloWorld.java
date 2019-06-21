d5714a5487aee7ba20e189d482a17c2aaab8ade7
ab2019-stuff <51882648+ab2019-stuff@users.noreply.github.com>
2019-06-21 08:28:43 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
  HEAD
 		System.out.println("Hello World!");
-		System.out.println("h");
+		System.out.println("hi");
 		System.out.println("I think I'll make a change here.");
  dev
 
